9f36646c040f275f23ce9b2c396e3d4d610358f9
Lewis Prescott <lewis.prescott@asos.com>
2018-02-04 16:50:04 +0000

    Add project files.

---
+++ Student/Models/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student.Models
+{
+    /// <summary> 
+    /// Standard class sealed due to virtual type initialized within constructor
+    /// </summary>
+    /// <value> 
+    /// StudentName is not null
+    /// </value>
+    public class Student
+    {
+        public int StudentId { get; set; }
+        [Required]
+        public string StudentName { get; set; }
+        public int StandardId { get; set; }
+        public virtual Standard Standard { get; set; }
+    }
+
+    /// <summary>
+    /// Standard class sealed due to virtual type initialized within constructor
+    /// </summary>
+    public sealed class Standard
+    {
+        public Standard()
+        {
+            Students = new List<Student>();
+        }
+
+        public int StandardId { get; set; }
+        public string StandardName { get; set; }
+        public string Description { get; set; }
+        public ICollection<Student> Students { get; set; }
+    }
+}
---
+++ Student/Models/StudentContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Student.Models
+{
+    public class StudentContext : DbContext
+    {
+        /// <inheritdoc />
+        /// <summary>
+        /// Student DbContext
+        /// </summary>
+        /// <param name="options"></param>
+        public StudentContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// ModelBuilder used with EnityFrameworkCore's Fluent API = DbModelBuilder in .Net Framework
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //one-to-many
+            // Foreign Key = StandardId
+            // Standard Is Required
+            modelBuilder.Entity<Student>().Property(s => s.Standard)
+                .IsRequired();
+            modelBuilder.Entity<Student>()
+                .HasMany(s => s.Standard.Students)
+                .WithOne().HasForeignKey(s => s.StandardId);
+        }
+    }
+}
